2509b5e058997869d1b3fb9e2e330659e57b23fb
srbberlin <info@hans-schrieber.de>
2018-02-15 14:24:01 +0100

    Fixed creation of rooms Added tunnels

--- src/babel/game/game-logic.js
+++ src/babel/game/game-logic.js
@@ -1,8 +1,8 @@
 import { GameElements } from './game-elements.js'
 
-var d = GameElements.d
-var m = GameElements.m
-var data = {
+let d = GameElements.d // Fieldtypes
+let m = GameElements.m // Constructors
+let data = {
   players: [
     m.player ('', 'me', 0, [], []),
     m.player ('', 'enemy', 0, [], []),
@@ -38,54 +38,72 @@ var data = {
     m.health('GameElements.healths.h3', 2),
     m.health('GameElements.healths.h3', 2),
     m.health('GameElements.healths.h3', 2),
-  ],
-  data: [
-    [82,  0, 92,  5],
-    [38, 20, 42, 40],
-    [38, 20, 42, 40],
-    [39, 18, 45, 38],
-    [28, 43, 46, 65],
-    [14, 60, 28, 66],
-    [ 3, 32, 11, 48],
-    [ 0,  7, 18, 13],
-    [38, 63, 60, 71],
-    [60, 68, 10, 14],
-    [38, 32, 48, 38],
-    [10, 44, 20, 62],
-    [35, 22, 47, 26],
-    [ 4,  4, 22, 18],
-    [72, 66, 88, 84],
-    [26, 87, 48, 99],
-    [42, 68, 46, 90],
-    [62, 18, 68, 22],
-    [86, 70, 99, 78],
-    [57, 54, 69, 62],
-    [14, 20, 30, 38],
-    [ 4, 79, 10, 85],
-    [79, 94, 89, 99],
-    [60, 42, 70, 48],
-    [14,  1, 22,  9],
-    [57, 56, 67, 60],
-    [66,  0, 76, 12],
-    [62, 87, 76, 99],
-    [13, 38, 29, 46],
-    [93,  4, 99, 10],
-    [14,  8, 22, 24],
   ]
 }
 
-var NewGame = {
+const Y1 = 0
+const X1 = 1
+const Y2 = 2
+const X2 = 3
+const Y  = 4
+const X  = 5
+
+let isIn = (c, f) => {
+  let a1 = c[X1] <= (f[X1] +1) && f[X1] <= (c[X2] +1)
+  let a2 = c[X1] <= (f[X2] +1) && f[X2] <= (c[X2] +1)
+  let a3 = c[Y1] <= (f[Y1] +1) && f[Y1] <= (c[Y2] +1)
+  let a4 = c[Y1] <= (f[Y2] +1) && f[Y2] <= (c[Y2] +1)
+        
+  if ((a1 || a2) && (a3 || a4)) {
+    return true
+  }
+  return false
+}
+
+let newCoords = (y, x, ey, ex) => {
+  let e1 = Math.floor(Math.random()*(ey ? ey : 10) + 2)
+  let e2 = Math.floor(Math.random()*(ex ? ex : 10) + 2)
+
+  let ymin, ymax
+  let xmin, xmax
+
+  ymin = y - e1
+  ymax = y + e1
+  if (ymax >= 100) {
+    ymax = 99
+  }
+  if (ymin < 0) {
+    ymin = 0
+  }
+
+  xmin = x - e2
+  xmax = x + e2
+  if (xmax >= 100) {
+    xmax = 99
+  }
+  if (xmin < 0) {
+    xmin = 0
+  }
+
+  return [ymin, xmin, ymax, xmax, y, x]
+}
+
+let NewGame = {
   field: (() => {
 
-    let pc = 0, wc = 0, hc = 0
-    let pcMax = data.players.length
-    let wcMax = data.weapons.length
-    let hcMax = data.healths.length
-    let y, x, o
-    let e1, e2
+    let pc = 0, pcMax = data.players.length
+    let wc = 0, wcMax = data.weapons.length
+    let hc = 0, hcMax = data.healths.length
+    let y, y1, y2, ym, x, x1, x2, xm, o
+    let coords
 
-    let f = [[],[],[]] // main data, shadow, roompatterns
-    let t = d.env[0].v // default type for now
+    let f = [
+      [], // main data
+      [], // shadow
+      []  // roompatterns
+    ]
+    let t = d.env[0].v // default type for now: wall
+
     for (y = 0; y < 100; y++) { // generate data structure
       f[0].push([])
       f[1].push([])
@@ -95,9 +113,14 @@ var NewGame = {
       }
     }
 
+    /*
+     * Position the combatants and welths
+     * 
+     */
+
     do {
-      y = Math.abs(Math.floor(Math.random() * 100))
-      x = Math.abs(Math.floor(Math.random() * 100))
+      y = Math.abs(Math.floor(Math.random() * 94 + 3))
+      x = Math.abs(Math.floor(Math.random() * 94 + 3))
       if (pc < pcMax) {
         o = data.players[pc++]
         t = d.env[1].v
@@ -114,7 +137,13 @@ var NewGame = {
       f[0][y][x][1] = o
       f[1][y][x][0] = t
       f[1][y][x][1] = o
+
     } while (hc < hcMax)
+
+    /*
+     * Provide the rooms of the combatants
+     * 
+     */
 
     pc = 0, wc = 0, hc = 0
     do {
@@ -130,43 +159,22 @@ var NewGame = {
       x = o.pos[1]
       t = d.env[1].v
 
-      e1 = Math.floor(Math.random()*10 + 2)
-      e2 = Math.floor(Math.random()*10 + 2)
+      coords = newCoords(y, x)
 
-      let ymin, y1, ymax
-      let xmin, x1, xmax
-
-      ymin = y - e1
-      ymax = y + e1
-      if (ymax >= 100) {
-        ymax = 99
-      }
-      if (ymin < 0) {
-        ymin = 0
-      }
-
-      xmin = x - e2
-      xmax = x + e2
-      if (xmax >= 100) {
-        xmax = 99
-      }
-      if (xmin < 0) {
-        xmin = 0
-      }
-
-      f[2].push([ymin, xmin, ymax, xmax])
+      f[2].push([coords]) // One element per row
             
-      y1 = ymin
-      while (y1 <= ymax) {
-        x1 = xmin
-        while (x1 <= xmax) {
+      y1 = coords[Y1]
+      ym = coords[Y2]
+      while (y1 <= ym) {
+        x1 = coords[X1]
+        xm = coords[X2]
+        while (x1 <= xm) {
           if (x1 !== x || y1 !== y) {
-            f[0][y1][x1][2]
             f[0][y1][x1][0] = t
             f[1][y1][x1][0] = t
           }
           else {
-            f[0][y1][x1][2] = [ymin, xmin, ymax, xmax]
+            f[0][y1][x1][2] = coords // Display
           }
           x1++
         }
@@ -174,110 +182,111 @@ var NewGame = {
       }
     } while (hc < hcMax)
 
-    const Y1 = 0
-    const X1 = 1
-    const Y2 = 2
-    const X2 = 3
-    
-    let isIn = (c, f) => {
-
-      if (
-        (
-          c[Y1] <= f[Y1] && f[Y1] <= c[Y2] ||
-          c[Y1] <= f[Y2] && f[Y2] <= c[Y2]
-        ) &&
-        (
-          f[X1] <= c[X2] && f[X2] >= c[X1]
-        )
-      ) {
-        console.log('Y', c[Y1],c[X1],c[Y2],c[X2],':',f[Y1],f[X1],f[Y2],f[X2])
-        return true
-      }
-      if (
-        (
-          c[X1] <= f[X1] && f[X1] <= c[X2] ||
-          c[X1] <= f[X2] && f[X2] <= c[X2]
-        ) &&
-        (
-          f[Y1] <= c[Y2] && f[Y2] >= c[Y1]
-        )
-      ) {
-        console.log('X', c[Y1],c[X1],c[Y2],c[X2],':',f[Y1],f[X1],f[Y2],f[X2])
-        return true
-      }
-      return false
-      /*
-      if (
-        c[Y1] <= f[Y1] && f[Y1] <= c[Y2] &&
-        c[X1] <= f[X1] && f[X1] <= c[X2]
-      ) {
-        //console.log('4', flag, c[Y1],'<',f[Y1],'<',c[Y2],'&&',c[X1],'<',f[X1],'<',c[X2])
-        return true
-      }
-      if (
-        c[Y1] <= f[Y1] && f[Y1] <= c[Y2] &&
-        c[X1] <= f[X2] && f[X2] <= c[X2]
-      ) {
-        //console.log('4', flag, c[Y1],'<',f[Y1],'<',c[Y2],'&&',c[X1],'<',f[X2],'<',c[X2])
-        return true
-      }
-      if (
-        c[Y1] <= f[Y2] && f[Y2] <= c[Y2] &&
-        c[X1] <= f[X2] && f[X2] <= c[X2]
-      ) {
-        //console.log('4', flag, c[Y1],'<',f[Y2],'<',c[Y2],'&&',c[X1],'<',f[X2],'<',c[X2])
-        return true
-      }
-      if (
-        c[Y1] <= f[Y2] && f[Y2] <= c[Y2] &&
-        c[X1] <= f[X1] && f[X1] <= c[X2]
-      ) {
-        //console.log('4', flag, c[Y1],'<',f[Y2],'<',c[Y2],'&&',c[X1],'<',f[X1],'<',c[X2])
-        return true
-      }
-      */
-    }
-
-    console.log(f[2])
-    f[2] = f[2].reduce((a, c) => {
-      if (a.length > 0) {
-        let i = 0
-        let l = a.length
-        while (i < l) {
-          let b = a[i++] // get the next group
-          let j = 0
-          let l = b.length
-          while (j < l) {
-            let d = b[j] // get the next room
-            if (
-              isIn(c, d) // is it overlapping with the current room ?
-            ) {
-              b.push(c) // add another room to the group
-              console.log('=', c[Y1],c[X1],c[Y2],c[X2])
-              return a
+    let la = 0, lb
+    do {
+      f[2] = f[2].reduce(
+        (a0, c1) => {
+          if (a0.length > 0) {
+            let i0 = 0
+            let l0 = a0.length
+            while (i0 < l0) {
+              let a1 = a0[i0]
+              let i1 = 0
+              let l1 = a1.length
+              while (i1 < l1) {
+                let a2 = a1[i1]
+                let i2 = 0
+                let l2 = c1.length
+                while (i2 < l2) {
+                  let c2 = c1[i2]
+                  if (isIn(a2, c2) || isIn(c2, a2)) {
+                    a0[i0] = a1.concat(c1)
+                    return a0
+                  }
+                  i2++
+                }
+                i1++
+              }
+              i0++
             }
-            j++
           }
-        }
-      }
-      console.log('+', c[Y1],c[X1],c[Y2],c[X2])
-      a.push([c]) // add another group with it's first room
-      return a
-    },
-    [])
-
-    f[2].sort((a, b) => {
-      return a.length - b.length
-    })
-
-    let c = 0, i = 0, l = f[2].length
-    while (i < l) {
-      let d = f[2][i++].length
-      c += d
-      console.log(i, d)
+          a0.push(c1)
+          return a0
+        },
+        []
+      )
+      lb = la
+      la = f[2].length
     }
-    console.log(l, c)
-    console.log(f[2])
+    while (la !== lb)
+    
+    f[2].sort((a, b) => { return a.length - b.length })
+
+    /*
+     * Connect rooms with tunnels
+     * 
+     */
+
+    la = 0
+    f[2].reduce(
+      (a, c, i) => {
+        if (i === 0) {
+          y1 = c[0][Y]
+          x1 = c[0][X]
+          a.push([])
+        }
+        else {
+          let tmp
+
+          y2 = c[0][Y]
+          x2 = c[0][X]
+          t = d.env[3].v
+
+          x = x1
+          y = y1
+          ym = y2
+          if (y > ym) {
+            tmp = ym
+            ym = y
+            y = tmp
+          }
+
+          while (y <= ym) {
+            if (f[0][y][x][0] === d.env[0].v) {
+              f[0][y][x][0] = t
+              f[0][y][x][1] = null
+              f[1][y][x][0] = t
+              f[1][y][x][1] = null
+            }
+            y++
+          }
+
+          y = y2
+          x = x1
+          xm = x2
+          if (x > xm) {
+            tmp = xm
+            xm = x
+            x = tmp
+          }
+
+          while (x <= xm) {
+            if (f[0][y][x][0] === d.env[0].v) {
+              f[0][y][x][0] = t
+              f[0][y][x][1] = null
+              f[1][y][x][0] = t
+              f[1][y][x][1] = null
+            }
+            x++
+          }
+          
+          y1 = y2
+          x1 = x2
+        }
+        return a
+      },
+      []
+    )
 
     return f
   }
